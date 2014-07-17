b12f972982b9875bb48917313a5a85e8783c373d
Rodelle Ladia Jr <rLadia@gmail.com>
2014-07-16 22:47:07 -0700

    Initial implementation of obstacles

--- Java/DyeHardAPI/src/Dyehard/Obstacles/Obstacle.java
+++ Java/DyeHardAPI/src/Dyehard/Obstacles/Obstacle.java
@@ -1,3 +1,41 @@
 package Dyehard.Obstacles;
-public class Obstacle {
+
+import java.awt.Color;
+import java.util.List;
+
+import Dyehard.Enemies.Enemy;
+import Dyehard.Player.Hero;
+import Dyehard.World.GameWorld;
+import Engine.Rectangle;
+import Engine.Vector2;
+
+public class Obstacle extends Rectangle {
+    private Hero hero;
+
+    public Obstacle(Hero hero, List<Enemy> enemies, Vector2 center,
+            float width, float height) {
+        // super(center, width, height);
+        this.center = center;
+        size = new Vector2(width, height);
+        shouldTravel = true;
+        velocity = new Vector2(-GameWorld.Speed, 0);
+        color = Color.GREEN;
+        this.hero = hero;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        checkCollisions();
+    }
+
+    private void checkCollisions() {
+        if (!isInsideWorldBound()) {
+            destroy();
+            return;
+        }
+        if (collided(hero.getPosition())) {
+            pushOutCircle(hero.getPosition());
+        }
+    }
 }
--- Java/DyeHardAPI/src/Dyehard/World/GameWorld.java
+++ Java/DyeHardAPI/src/Dyehard/World/GameWorld.java
@@ -1,30 +1,31 @@
 package Dyehard.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Dyehard.DeveloperControls;
 import Dyehard.GameObject;
+import Dyehard.Obstacles.Obstacle;
 import Dyehard.Player.Hero;
 import Engine.KeyboardInput;
+import Engine.Primitive;
 import Engine.Vector2;
 
 public class GameWorld extends GameObject {
     // private final float StartSpeed = 0.2f;
-    public static float Speed;
+    public static float Speed = .05f;
     public static Vector2 Gravity = new Vector2(0, -0.02f);
-    // private float SpeedReference;
-    // private boolean stop;
-    // private Timer accelerationTimer;
-    //
-    // private Queue<GameWorldRegion> onscreen;
-    // private Queue<GameWorldRegion> upcoming;
     private Hero hero;
     private DeveloperControls dev;
+    private List<Primitive> primitives;
 
-    // private EnemyManager eManager;
-    // private InfoPanel infoPanel;
-    // private Laser laser;
     public GameWorld(KeyboardInput keyboard) {
         hero = new Hero(keyboard);
         dev = new DeveloperControls(keyboard, hero);
+        primitives = new ArrayList<Primitive>();
+        primitives.add(new Obstacle(hero, null, new Vector2(50, 10), 5, 5));
+        primitives.add(new Obstacle(hero, null, new Vector2(55, 25), 5, 5));
+        primitives.add(new Obstacle(hero, null, new Vector2(60, 40), 5, 5));
     }
 
     @Override
@@ -45,5 +46,13 @@ public class GameWorld extends GameObject {
     public void update() {
         hero.update();
         dev.update();
+        for (Primitive p : primitives) {
+            p.update();
+        }
+        for (int i = primitives.size() - 1; i >= 0; --i) {
+            if (primitives.get(i).visible == false) {
+                primitives.remove(i);
+            }
+        }
     }
 }
