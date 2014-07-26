968b00c01d9fdce7eaad822dd7a821c5659ebfaa
John Louie <gotacoolemail@yahoo.com>
2014-07-25 20:22:31 -0700

    Add LimitedAmmoWeapon class

--- Java/DyeHardAPI/src/Dyehard/Weapons/LimitedAmmoWeapon.java
+++ Java/DyeHardAPI/src/Dyehard/Weapons/LimitedAmmoWeapon.java
@@ -1,4 +1,52 @@
 package Dyehard.Weapons;
 
-public class LimitedAmmoWeapon {
+import java.awt.Color;
+
+import BaseTypes.Weapon;
+import Dyehard.Player.Hero;
+import Dyehard.World.GameWorld;
+import Engine.Rectangle;
+import Engine.Vector2;
+
+public class LimitedAmmoWeapon extends Weapon {
+    private int reloadAmount = 10;
+    private int ammo;
+    private Rectangle ammoTracker;
+
+    public LimitedAmmoWeapon(Hero hero) {
+        super(hero);
+        ammo = 10;
+        ammoTracker = new Rectangle();
+        ammoTracker.center = new Vector2(GameWorld.LEFT_EDGE + 8,
+                GameWorld.TOP_EDGE - 4);
+        ammoTracker.size.set(4, 4);
+    }
+
+    public void recharge() {
+        ammo = reloadAmount;
+    }
+
+    @Override
+    public void update() {
+        if (ammo == 0) {
+            ammoTracker.color = Color.RED;
+        } else {
+            ammoTracker.color = Color.GREEN;
+        }
+        super.update();
+    }
+
+    @Override
+    public void fire() {
+        if (ammo > 0) {
+            super.fire();
+            ammo--;
+        }
+    }
+
+    @Override
+    public void draw() {
+        ammoTracker.draw();
+        super.draw();
+    }
 }
