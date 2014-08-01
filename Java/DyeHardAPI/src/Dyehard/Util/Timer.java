9c2859da334af8cc558cdd270f041ea96a44cb5e
John Louie <gotacoolemail@yahoo.com>
2014-07-31 22:19:45 -0700

    Add Timer class

--- Java/DyeHardAPI/src/Dyehard/Util/Timer.java
+++ Java/DyeHardAPI/src/Dyehard/Util/Timer.java
@@ -1,3 +1,26 @@
 package Dyehard.Util;
+
 public class Timer {
+    // All times are in nanoseconds
+    private float endTime;
+    private float interval;
+
+    public Timer(float milliSeconds) {
+        // Converting milliseconds to nanoseconds
+        interval = milliSeconds * 1000000;
+        endTime = System.nanoTime() + interval;
+    }
+
+    public boolean isDone() {
+        return System.nanoTime() >= endTime;
+    }
+
+    public void reset() {
+        endTime = System.nanoTime() + interval;
+    }
+
+    public float timeRemaining() {
+        // Returns the amount of time left in milliseconds
+        return (endTime - System.nanoTime()) / 1000000;
+    }
 }
