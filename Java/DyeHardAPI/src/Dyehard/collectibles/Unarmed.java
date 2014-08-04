ccf2cdd01751f7e14bfe06670ff03a1c058204e3
John Louie <gotacoolemail@yahoo.com>
2014-08-04 16:59:58 -0700

    Added Unarmed

---
+++ Java/DyeHardAPI/src/Dyehard/collectibles/Unarmed.java
@@ -0,0 +1,20 @@
+package dyehard.Collectibles;
+
+import java.awt.Color;
+
+import dyehard.Player.Hero;
+
+public class Unarmed extends PowerUp {
+    public Unarmed(Hero hero, float minX, float maxX) {
+        super(hero, minX, maxX);
+        color = Color.cyan;
+        // texture = BaseCode.resources.loadImage("Textures/PowerUp_Blue.png");
+    }
+
+    @Override
+    public void activate() {
+        hero.unarmedOn();
+        PowerUpManager.UnarmedTimer.reset();
+        super.activate();
+    }
+}
