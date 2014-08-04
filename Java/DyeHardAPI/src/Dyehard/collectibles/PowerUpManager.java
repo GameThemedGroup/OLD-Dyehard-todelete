ec3b5fe4d8d67703ceab12ad064c6bde82d7c7d6
John Louie <gotacoolemail@yahoo.com>
2014-08-04 10:23:00 -0700

    Add SpeedUp support to PowerUpManager

---
+++ Java/DyeHardAPI/src/Dyehard/collectibles/PowerUpManager.java
@@ -0,0 +1,26 @@
+package dyehard.Collectibles;
+
+import dyehard.Player.Hero;
+import dyehard.Util.Timer;
+
+public class PowerUpManager {
+    public static Timer SpeedUpTimer;
+    public static Timer GhostTimer;
+    public static Timer OverloadTimer;
+    public static Timer InvincibilityTimer;
+    private Hero hero;
+
+    public PowerUpManager(Hero hero) {
+        SpeedUpTimer = new Timer(PowerUp.DURATION);
+        GhostTimer = new Timer(PowerUp.DURATION);
+        OverloadTimer = new Timer(PowerUp.DURATION);
+        InvincibilityTimer = new Timer(PowerUp.DURATION);
+        this.hero = hero;
+    }
+
+    public void update() {
+        if (SpeedUpTimer.isDone()) {
+            hero.normalizeSpeed();
+        }
+    }
+}
