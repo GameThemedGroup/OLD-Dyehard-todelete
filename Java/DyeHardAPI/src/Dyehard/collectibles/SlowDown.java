63811fb9d1b5b196ff40bd3b215ae302333202e8
John Louie <gotacoolemail@yahoo.com>
2014-08-04 18:45:35 -0700

    Add SlowDown power up

---
+++ Java/DyeHardAPI/src/Dyehard/collectibles/SlowDown.java
@@ -0,0 +1,28 @@
+package dyehard.Collectibles;
+
+import java.awt.Color;
+import java.util.List;
+
+import dyehard.Enemies.Enemy;
+import dyehard.Player.Hero;
+
+public class SlowDown extends PowerUp {
+    List<Enemy> enemies;
+
+    public SlowDown(Hero hero, List<Enemy> enemies, float minX, float maxX) {
+        super(hero, minX, maxX);
+        this.enemies = enemies;
+        color = Color.gray;
+        // texture = BaseCode.resources.loadImage("Textures/PowerUp_Green.png");
+    }
+
+    @Override
+    public void activate() {
+        for (Enemy e : enemies) {
+            e.normalizeSpeed();
+            e.decreaseSpeed();
+        }
+        PowerUpManager.EnemySpeedTimer.reset();
+        super.activate();
+    }
+}
