b40dd085bb66d3ed335fd8db84fe48b4dd4d2c05
John Louie <gotacoolemail@yahoo.com>
2014-08-04 20:54:50 -0700

    Add Magnetism power up

---
+++ Java/DyeHardAPI/src/Dyehard/collectibles/Magnetism.java
@@ -0,0 +1,20 @@
+package dyehard.Collectibles;
+
+import java.awt.Color;
+
+import dyehard.Player.Hero;
+
+public class Magnetism extends PowerUp {
+    public Magnetism(Hero hero, float minX, float maxX) {
+        super(hero, minX, maxX);
+        color = Color.white;
+        // texture = BaseCode.resources.loadImage("Textures/PowerUp_Pink.png");
+    }
+
+    @Override
+    public void activate() {
+        hero.magnetismOn();
+        PowerUpManager.MagnetismTimer.reset();
+        super.activate();
+    }
+}
