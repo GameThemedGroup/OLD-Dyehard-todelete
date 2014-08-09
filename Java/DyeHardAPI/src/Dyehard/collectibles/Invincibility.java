6f6254a3241c1abd75e74d671542b756f0638d8d
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-08-09 12:58:53 -0700

    Implement invincibility

--- Java/DyeHardAPI/src/Dyehard/collectibles/Invincibility.java
+++ Java/DyeHardAPI/src/Dyehard/collectibles/Invincibility.java
@@ -1,24 +1,33 @@
 package dyehard.Collectibles;
 
 import Engine.BaseCode;
+import Engine.Primitive;
 import dyehard.Player.Hero;
+import dyehard.Player.HeroInterfaces.HeroDamage;
 
 public class Invincibility extends PowerUp {
     // public static PowerUpMeter meter = new PowerUpMeter(2, Game.Pink);
     public Invincibility() {
         texture = BaseCode.resources.loadImage("Textures/PowerUp_Pink.png");
+        applicationOrder = 1000;
     }
 
     @Override
     public void activate(Hero hero) {
         // meter.reset(Duration, null);
+        System.out.println("Activated invincibility");
         super.activate(hero);
     }
 
     @Override
     public void apply(Hero hero) {
-        // TODO Auto-generated method stub
-
+        hero.damageHandler = new HeroDamage() {
+            @Override
+            public void damageHero(Hero hero, Primitive who) {
+                // The hero becomes immune to damage
+                return;
+            }
+        };
     }
 
     @Override
