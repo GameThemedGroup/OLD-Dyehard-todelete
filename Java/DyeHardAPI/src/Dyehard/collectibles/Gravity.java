551db8691ab926f92d9dd0e98b6c83bdf87a7ce4
John Louie <gotacoolemail@yahoo.com>
2014-08-09 20:20:54 -0700

    Add Gravity class

---
+++ Java/DyeHardAPI/src/Dyehard/collectibles/Gravity.java
@@ -0,0 +1,21 @@
+package dyehard.Collectibles;
+
+import java.awt.Color;
+
+import dyehard.Player.Hero;
+
+public class Gravity extends PowerUp {
+    public Gravity(Hero hero, float minX, float maxX) {
+        super(hero, minX, maxX);
+        color = Color.red;
+        // texture = BaseCode.resources.loadImage("Textures/PowerUp_Blue.png");
+    }
+
+    @Override
+    public void activate() {
+        hero.gravityOn();
+        PowerUpManager.GravityTimer.reset();
+        super.activate();
+        System.out.println("Picked up Gravity");
+    }
+}
