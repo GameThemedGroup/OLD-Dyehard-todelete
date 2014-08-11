ff2abb6001761a9d71a5b7a5b1e523af0fb2a6b9
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-08-10 22:03:00 -0700

    Add ship background graphic

--- Java/DyeHardAPI/src/Dyehard/background/Background.java
+++ Java/DyeHardAPI/src/Dyehard/background/Background.java
@@ -2,6 +2,7 @@ package dyehard.Background;
 
 import java.awt.Color;
 
+import Engine.BaseCode;
 import Engine.Rectangle;
 import Engine.Vector2;
 import dyehard.World.GameWorld;
@@ -11,31 +12,35 @@ public class Background {
     private Rectangle backdrop;
     private Starfield foreground;
     private Starfield background;
-    private InfinityShip ship;
 
     public Background() {
         float width = GameWorld.RIGHT_EDGE - GameWorld.LEFT_EDGE;
         float height = GameWorld.TOP_EDGE - GameWorld.BOTTOM_EDGE;
 
+        // The infinity ship currently consists of 4 game tiles
+        width *= 4;
+
         // Create a black background to represent space
         backdrop = new Rectangle();
         Vector2 center = new Vector2(width / 2, height / 2);
+
         backdrop.center = center;
+        backdrop.texture = BaseCode.resources
+                .loadImage("Textures/ship_background.png");
+        backdrop.velocity = new Vector2(-0.04f, 0f);
         backdrop.size = new Vector2(width, height);
         backdrop.color = Color.BLACK;
-        backdrop.shouldTravel = false;
+        backdrop.shouldTravel = true;
 
         // TODO replace these magic numbers
         foreground = new Starfield(.8f, .2f, 30f);
         background = new Starfield(.4f, .1f, 10f);
-        ship = new InfinityShip(0.04f);
     }
 
     public void update() {
         backdrop.update();
         background.update();
         foreground.update();
-        ship.update();
     }
 
 }
