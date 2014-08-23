fa80ea8410c3c336a42eb6ba5e9c9ab1076e8d11
John Louie <gotacoolemail@yahoo.com>
2014-08-23 01:32:50 -0700

    Add Gravity class

---
+++ Java/DyeHardAPI/src/dyehard/Collectibles/Gravity.java
@@ -0,0 +1,32 @@
+package dyehard.Collectibles;
+
+import dyehard.Player.Hero;
+
+public class Gravity extends PowerUp {
+
+    public Gravity() {
+        applicationOrder = 2;
+        label.setText("Gravity");
+    }
+
+    @Override
+    public void apply(Hero hero) {
+        hero.currentGravity.set(0f, -1.5f);
+    }
+
+    @Override
+    public void unapply(Hero hero) {
+        hero.currentGravity = hero.defaultGravity;
+    }
+
+    @Override
+    public PowerUp clone() {
+        return new Gravity();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Gravity";
+    }
+
+}
