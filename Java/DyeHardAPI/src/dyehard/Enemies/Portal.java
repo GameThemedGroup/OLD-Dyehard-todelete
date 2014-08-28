edbcf5aebd4a3c3f105c47c13da30d5e3a9a3c65
John Louie <gotacoolemail@yahoo.com>
2014-08-28 14:29:56 -0700

    Add PortalEnemy's ability to spawn Portals

---
+++ Java/DyeHardAPI/src/dyehard/Enemies/Portal.java
@@ -0,0 +1,42 @@
+package dyehard.Enemies;
+
+import java.awt.Color;
+import java.util.Random;
+
+import Engine.BaseCode;
+import Engine.Vector2;
+import dyehard.GameObject;
+import dyehard.Player.Hero;
+import dyehard.Util.Timer;
+import dyehard.World.GameWorld;
+
+public class Portal extends GameObject {
+    private static float widthToHeightRatio = 1f;
+    private Hero hero;
+    private Timer timer;
+
+    public Portal(Vector2 center, float height, Hero hero) {
+        this.center = center.clone();
+        size.set(height * widthToHeightRatio, height);
+        this.hero = hero;
+        texture = BaseCode.resources.loadImage("Textures/PowerUp_Box1.png");
+        velocity = new Vector2(-GameWorld.Speed, 0f);
+        color = Color.black;
+        this.hero = hero;
+        timer = new Timer(5000f);
+    }
+
+    @Override
+    public void update() {
+        if (collided(hero)) {
+            Random rand = new Random();
+            hero.center.set(rand.nextInt(90) + 5, rand.nextInt(60));
+            hero.velocity = new Vector2(0f, 0f);
+        }
+        if (timer.isDone()) {
+            destroy();
+        }
+
+        super.update();
+    }
+}
--- Java/DyeHardAPI/src/dyehard/Enemies/PortalEnemy.java
+++ Java/DyeHardAPI/src/dyehard/Enemies/PortalEnemy.java
@@ -2,13 +2,25 @@ package dyehard.Enemies;
 
 import Engine.Vector2;
 import dyehard.Player.Hero;
+import dyehard.Util.Timer;
 
 public class PortalEnemy extends Enemy {
     private static float widthToHeightRatio = 1f;
+    private Timer timer;
 
     public PortalEnemy(Vector2 center, float height, Hero currentHero) {
         super(center, height * widthToHeightRatio, height, currentHero,
                 "Textures/Enemies/minion_Portal.png");
+        timer = new Timer(2000f);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (timer.isDone()) {
+            Portal blackHole = new Portal(center.clone(), 4f, hero);
+            timer.reset();
+        }
     }
 
     @Override
