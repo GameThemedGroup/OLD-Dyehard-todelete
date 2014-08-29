c7396a0c4d65b150bef0e9a53eb139ae43f27565
John Louie <gotacoolemail@yahoo.com>
2014-08-28 17:15:01 -0700

    Update UserCode

--- Java/DyeHardUser/src/UserCode.java
+++ Java/DyeHardUser/src/UserCode.java
@@ -2,7 +2,6 @@ import java.awt.event.KeyEvent;
 
 import dyehard.DyeHard;
 import dyehard.Collectibles.DyePack;
-import dyehard.Collectibles.JohnsBallinAssPowerUp;
 import dyehard.Player.Hero;
 import dyehard.Util.Colors;
 import dyehard.Weapons.LimitedAmmoWeapon;
@@ -22,19 +21,20 @@ public class UserCode extends DyeHard {
         hero.registerWeapon(new OverHeatWeapon(hero));
         hero.registerWeapon(new LimitedAmmoWeapon(hero));
 
-        Space.addDefaultPowerUps(10);
+        Space.addDefaultPowerUps(5);
 
-        JohnsBallinAssPowerUp john = new JohnsBallinAssPowerUp();
-        john.center.set(50f, 30f);
-        Space.registerPowerUp(john);
+        /*
+         * JohnsBallinAssPowerUp john = new JohnsBallinAssPowerUp();
+         * john.center.set(50f, 30f); Space.registerPowerUp(john);
+         */
 
-        Space.addDefaultDyePacks(10);
+        Space.addDefaultDyePacks(11);
 
         DyePack p = new DyePack(Colors.Yellow);
         p.center.set(60f, 30f);
         Space.registerDyePack(p);
 
-        Space.addDefaultDebris(15);
+        Space.addDefaultDebris(10);
 
         world.initialize(hero);
 
@@ -44,21 +44,15 @@ public class UserCode extends DyeHard {
     @Override
     protected void update() {
         /*
-         * switch (state) { case PAUSED: CollisionManager.update();
-         * hero.update(); devControls.update(); break; default: break; }
+         * if (keyboard.isButtonDown(KeyEvent.VK_U)) { JohnsBallinAssPowerUp
+         * john = new JohnsBallinAssPowerUp();
+         * john.center.set(hero.center.getX() + 10f, hero.center.getY());
+         * Space.registerPowerUp(john); }
+         * 
+         * if (keyboard.isButtonDown(KeyEvent.VK_I)) { DyePack p = new
+         * DyePack(Colors.Yellow); p.center.set(hero.center.getX() + 10f,
+         * hero.center.getY()); Space.registerDyePack(p); }
          */
-
-        if (keyboard.isButtonDown(KeyEvent.VK_U)) {
-            JohnsBallinAssPowerUp john = new JohnsBallinAssPowerUp();
-            john.center.set(hero.center.getX() + 10f, hero.center.getY());
-            Space.registerPowerUp(john);
-        }
-
-        if (keyboard.isButtonDown(KeyEvent.VK_I)) {
-            DyePack p = new DyePack(Colors.Yellow);
-            p.center.set(hero.center.getX() + 10f, hero.center.getY());
-            Space.registerDyePack(p);
-        }
 
         if (keyboard.isButtonDown(KeyEvent.VK_UP)) {
             hero.moveUp();
