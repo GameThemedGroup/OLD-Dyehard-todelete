00af7732d511c3495b173e04dce78f6183f48b4d
Rodelle Ladia Jr. <rLadia@gmail.com>
2014-08-29 11:20:18 -0700

    Initial commit of progress bar

--- Java/DyeHardAPI/src/dyehard/Background/DyehardUI.java
+++ Java/DyeHardAPI/src/dyehard/Background/DyehardUI.java
@@ -1,6 +1,7 @@
 package dyehard.Background;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,18 +11,20 @@ import Engine.Text;
 import Engine.Vector2;
 import dyehard.UpdateObject;
 import dyehard.Player.Hero;
+import dyehard.World.GameState;
 import dyehard.World.GameWorld;
 
 public class DyehardUI extends UpdateObject {
     protected Hero hero;
     Rectangle frame;
-    Rectangle progress;
+    DyehardProgressBar progress;
     Text scoreText;
     List<Rectangle> hearts = new ArrayList<Rectangle>();
 
+    final Vector2 screenSize = new Vector2(1920, 1080);
+
     public DyehardUI(Hero hero) {
 
-        Vector2 screenSize = new Vector2(1920, 1080);
         this.hero = hero;
         frame = new Rectangle();
         String path = "Textures/UI/Dyehard_UI_HudFrame.png";
@@ -34,16 +37,7 @@ public class DyehardUI extends UpdateObject {
         frame.center = new Vector2(GameWorld.RIGHT_EDGE / 2, GameWorld.TOP_EDGE
                 - frame.size.getY() / 2);
 
-        progress = new Rectangle();
-        path = "Textures/UI/Dyehard_UI_Progress_Path.png";
-        progress.texture = BaseCode.resources.loadImage(path);
-
-        // TODO remove magic numbers
-        progress.size = scaleToGameWorld(screenSize, new Vector2(1104, 54));
-
-        // TODO remove magic numbers
-        progress.center = new Vector2(GameWorld.RIGHT_EDGE / 2,
-                GameWorld.TOP_EDGE - progress.size.getY() / 2 - 1.5f);
+        progress = new DyehardProgressBar(GameState.TargetDistance);
 
         hearts = new ArrayList<Rectangle>();
         path = "Textures/UI/dyehard_ui_health_full.png";
@@ -70,6 +64,85 @@ public class DyehardUI extends UpdateObject {
         scoreText.setFontName("Arial");
     }
 
+    private class DyehardProgressBar {
+        protected int maxValue;
+        protected int currentValue;
+        protected Rectangle progress;
+
+        List<Rectangle> markers;
+        Rectangle dyeMarker;
+        BufferedImage filledMarker;
+
+        public DyehardProgressBar(int maxValue) {
+            this.maxValue = maxValue;
+            currentValue = 0;
+
+            // TODO remove magic numbers
+            String progressTexturePath = "Textures/UI/Dyehard_UI_Progress_Path.png";
+            progress = scaledRectangle(screenSize, new Vector2(1104, 54),
+                    progressTexturePath);
+
+            // TODO remove magic numbers
+            progress.center = new Vector2(GameWorld.RIGHT_EDGE / 2,
+                    GameWorld.TOP_EDGE - progress.size.getY() / 2 - 1.4f);
+
+            String markerTexturePath = "Textures/UI/Dyehard_UI_Progress_marker_empty.png";
+            Rectangle baseMarker = scaledRectangle(screenSize, new Vector2(76,
+                    76), markerTexturePath);
+
+            markers = new ArrayList<Rectangle>();
+
+            // first gate at 500 and every 900 afterwards
+            // TODO magic numbers
+            for (int i = 500; i < maxValue; i += 900) {
+                Rectangle marker = new Rectangle(baseMarker);
+                marker.center.setY(GameWorld.TOP_EDGE - marker.size.getY() / 2
+                        - 0.9f);
+                marker.center.setX(toWorldUnits(i));
+                markers.add(marker);
+            }
+
+            filledMarker = BaseCode.resources
+                    .loadImage("Textures/UI/Dyehard_UI_Progress_marker_full.png");
+
+            String dyeMarkerTexturePath = "Textures/UI/Dyehard_UI_Progress_Dye.png";
+            dyeMarker = scaledRectangle(screenSize, new Vector2(76, 76),
+                    dyeMarkerTexturePath);
+
+            // TODO magic numbers
+            dyeMarker.center.setY(GameWorld.TOP_EDGE - dyeMarker.size.getY()
+                    / 2 - 0.9f);
+
+            setValue(currentValue);
+        }
+
+        public void setValue(int value) {
+            currentValue = value;
+            dyeMarker.center.setX(toWorldUnits(value));
+
+            for (Rectangle r : markers) {
+                if (dyeMarker.center.getX() > r.center.getX()) {
+                    r.texture = filledMarker;
+                }
+            }
+        }
+
+        protected float toWorldUnits(int value) {
+            float scale = progress.size.getX() / maxValue;
+            float startX = progress.center.getX() - progress.size.getX() / 2f;
+
+            return startX + value * scale;
+        }
+    }
+
+    public static Rectangle scaledRectangle(Vector2 screenSize,
+            Vector2 rectSize, String texturePath) {
+        Rectangle rect = new Rectangle();
+        rect.texture = BaseCode.resources.loadImage(texturePath);
+        rect.size = scaleToGameWorld(screenSize, rectSize);
+        return rect;
+    }
+
     @Override
     public boolean isActive() {
         return true;
@@ -77,7 +150,8 @@ public class DyehardUI extends UpdateObject {
 
     @Override
     public void update() {
-        scoreText.setText("123456");
+        scoreText.setText(Integer.toString(GameState.Score));
+        progress.setValue(GameState.DistanceTravelled);
     }
 
     /**
