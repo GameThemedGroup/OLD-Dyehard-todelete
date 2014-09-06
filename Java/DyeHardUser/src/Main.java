1e255320d7157ecee666a766b225d165698c0dce
John Louie <gotacoolemail@yahoo.com>
2014-09-06 02:51:47 -0700

    Main reads from xml files

--- Java/DyeHardUser/src/Main.java
+++ Java/DyeHardUser/src/Main.java
@@ -1,10 +1,12 @@
 import Engine.GameWindow;
+import dyehard.Configuration;
 
 public class Main extends GameWindow {
     private static final long serialVersionUID = 1L;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         (new Main()).startProgram();
+        new Configuration();
     }
 
     public Main() {
