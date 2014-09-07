1904002b01ee162b3382004fbfa4c2b2a89a9148
John Louie <gotacoolemail@yahoo.com>
2014-09-06 17:03:38 -0700

    Add parsing for hero data

--- Java/DyeHardAPI/src/dyehard/Configuration.java
+++ Java/DyeHardAPI/src/dyehard/Configuration.java
@@ -21,18 +21,25 @@ import dyehard.Enemies.ShootingEnemy;
 public class Configuration {
     private DocumentBuilderFactory factory;
     private DocumentBuilder builder;
-    private Document document;
+
+    // Hero variables
+    public static float heroWidth;
+    public static float heroHeight;
+    public static float heroJetSpeed;
+    public static float heroSpeedLimit;
+    public static float heroDrag;
 
     public Configuration() throws Exception {
         factory = DocumentBuilderFactory.newInstance();
         builder = factory.newDocumentBuilder();
-        document = builder.parse(ClassLoader
-                .getSystemResourceAsStream("resources/Enemies.xml"));
 
-        parseDocument();
+        parseEnemyData();
+        parseHeroData();
     }
 
-    public void parseDocument() {
+    private void parseEnemyData() throws Exception {
+        Document document = builder.parse(ClassLoader
+                .getSystemResourceAsStream("resources/Enemies.xml"));
 
         NodeList nodeList = document.getDocumentElement().getChildNodes();
 
@@ -70,6 +77,39 @@ public class Configuration {
                     CollectorEnemy.setAttributes(width, height, sleepTimer,
                             speed);
                 }
+            }
+        }
+    }
+
+    private void parseHeroData() throws Exception {
+        Document document = builder.parse(ClassLoader
+                .getSystemResourceAsStream("resources/Hero.xml"));
+
+        NodeList nodeList = document.getDocumentElement().getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) node;
+
+                heroWidth = Float.parseFloat(elem.getElementsByTagName("width")
+                        .item(0).getChildNodes().item(0).getNodeValue());
+
+                heroHeight = Float.parseFloat(elem
+                        .getElementsByTagName("height").item(0).getChildNodes()
+                        .item(0).getNodeValue());
+
+                heroJetSpeed = Float.parseFloat(elem
+                        .getElementsByTagName("jetSpeed").item(0)
+                        .getChildNodes().item(0).getNodeValue());
+
+                heroSpeedLimit = Float.parseFloat(elem
+                        .getElementsByTagName("speedLimit").item(0)
+                        .getChildNodes().item(0).getNodeValue());
+
+                heroDrag = Float.parseFloat(elem.getElementsByTagName("drag")
+                        .item(0).getChildNodes().item(0).getNodeValue());
             }
         }
     }
