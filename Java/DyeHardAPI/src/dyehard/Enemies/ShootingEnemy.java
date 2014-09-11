f2dde8208855a6fac222ec4dda957f5a90ae617c
John Louie <gotacoolemail@yahoo.com>
2014-09-11 15:20:19 -0700

    Rename shooing enemy enum

--- Java/DyeHardAPI/src/dyehard/Enemies/ShootingEnemy.java
+++ Java/DyeHardAPI/src/dyehard/Enemies/ShootingEnemy.java
@@ -10,10 +10,10 @@ public class ShootingEnemy extends Enemy {
     public ShootingEnemy(Vector2 center, Hero currentHero) {
         super(center, 0, 0, currentHero, "Textures/Enemies/minion_shooter.png");
 
-        width = Configuration.getEnemyData(EnemyType.EN_SHOOTING).width;
-        height = Configuration.getEnemyData(EnemyType.EN_SHOOTING).height;
-        sleepTimer = Configuration.getEnemyData(EnemyType.EN_SHOOTING).sleepTimer * 1000f;
-        speed = Configuration.getEnemyData(EnemyType.EN_SHOOTING).speed;
+        width = Configuration.getEnemyData(EnemyType.SHOOTING_ENEMY).width;
+        height = Configuration.getEnemyData(EnemyType.SHOOTING_ENEMY).height;
+        sleepTimer = Configuration.getEnemyData(EnemyType.SHOOTING_ENEMY).sleepTimer * 1000f;
+        speed = Configuration.getEnemyData(EnemyType.SHOOTING_ENEMY).speed;
     }
 
     @Override
