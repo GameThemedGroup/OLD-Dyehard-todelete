8d213f5325419e61598ab0b6b69d1e053768d43a
Chuan Wang <chuan.wang101@gmail.com>
2014-10-31 08:04:00 -0700

    Change texture of mouse

--- Java/DyeHardAPI/src/dyehard/DyeHard.java
+++ Java/DyeHardAPI/src/dyehard/DyeHard.java
@@ -1,5 +1,10 @@
 package dyehard;
 
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+
 import Engine.LibraryCode;
 
 public abstract class DyeHard extends LibraryCode {
@@ -17,7 +22,11 @@ public abstract class DyeHard extends LibraryCode {
     @Override
     public void initializeWorld() {
         super.initializeWorld();
-
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.getImage("resources/Beak.png");
+        Cursor cursor = toolkit.createCustomCursor(image, new Point(0, 0),
+                "cursor");
+        window.setCursor(cursor);
         initialize(); // call user code Initialize()
     }
 
