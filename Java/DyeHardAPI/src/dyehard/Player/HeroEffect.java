49fd61cbbb231c386604914ee5e353ec2c3aaa59
Chuan Wang <chuan.wang101@gmail.com>
2014-11-07 04:04:53 -0800

    New sprites for boost effect implemented

--- Java/DyeHardAPI/src/dyehard/Player/HeroEffect.java
+++ Java/DyeHardAPI/src/dyehard/Player/HeroEffect.java
@@ -1,15 +1,32 @@
 package dyehard.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import Engine.BaseCode;
 import Engine.Vector2;
 import dyehard.DyehardRectangle;
+import dyehard.Player.Hero.Direction;
 
 public class HeroEffect {
+    public class Offset {
+        public Offset(float rotation, float x, float y) {
+            this.rotation = rotation;
+            transform = new Vector2(x, y);
+        }
+
+        public float rotation;
+        public Vector2 transform;
+    }
+
     private final Hero hero;
 
     private final DyehardRectangle repelLeft;
     private final DyehardRectangle repelRight;
     private final DyehardRectangle invin;
+    private final DyehardRectangle boost;
+
+    protected Map<Direction, Offset> offsets;
 
     public HeroEffect(Hero hero) {
         this.hero = hero;
@@ -41,9 +58,36 @@ public class HeroEffect {
         invin.setUsingSpriteSheet(true);
         invin.setSpriteSheet(invin.texture, 200, 300, 10, 2);
         invin.visible = false;
+
+        // Constant offset for all directions
+        offsets = new HashMap<Direction, Offset>();
+        offsets.put(Direction.UP, new Offset(90f, -hero.size.getX() * 0.3f,
+                -hero.size.getY() * 0.2f));
+        offsets.put(Direction.DOWN, new Offset(-90f, -hero.size.getX() * 0.3f,
+                hero.size.getY() * 0.2f));
+        offsets.put(Direction.LEFT, new Offset(180f, hero.size.getX() / 1.8f,
+                hero.size.getY() * 0.1f));
+        // offsets.put(Direction.RIGHT, new Offset(30, -1, 1));
+        offsets.put(Direction.NEUTRAL, new Offset(0f, -hero.size.getX() / 1.6f,
+                hero.size.getY() * 0.1f));
+
+        boost = new DyehardRectangle();
+        Offset offset = offsets.get(hero.directionState);
+        boost.rotate = offset.rotation;
+        boost.center = hero.center.clone().add(offset.transform);
+        boost.size = new Vector2(2f, 2f);
+        boost.texture = BaseCode.resources
+                .loadImage("Textures/Hero/Dye_boost_AnimSheet.png");
+        boost.setUsingSpriteSheet(true);
+        boost.setSpriteSheet(boost.texture, 40, 40, 8, 2);
+        boost.visible = true;
     }
 
     public void update() {
+        Offset offset = offsets.get(hero.directionState);
+        boost.rotate = offset.rotation;
+        boost.center = hero.center.clone().add(offset.transform);
+
         if (hero.isInvin) {
             if (!invin.visible) {
                 invin.visible = true;
@@ -77,6 +121,8 @@ public class HeroEffect {
         repelLeft.addToAutoDrawSet();
         repelRight.removeFromAutoDrawSet();
         repelRight.addToAutoDrawSet();
+        boost.removeFromAutoDrawSet();
+        boost.addToAutoDrawSet();
     }
 
 }
