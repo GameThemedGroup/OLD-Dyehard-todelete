1732a9c72dfd71a4bda6b5045dbcfdb61f8aa331
Chuan Wang <chuan.wang101@gmail.com>
2014-11-20 20:21:03 -0800

    Revert "Revert "Change laser animation and fix panning code""
    This reverts commit 066e3d7da08091631d48794185a8244498857eda.

--- Java/DyeHardAPI/src/dyehard/Obstacles/Laser.java
+++ Java/DyeHardAPI/src/dyehard/Obstacles/Laser.java
@@ -2,11 +2,13 @@ package dyehard.Obstacles;
 
 import Engine.BaseCode;
 import Engine.Vector2;
+import dyehard.DyehardRectangle;
 import dyehard.GameObject;
 import dyehard.Player.Hero;
 
 public class Laser extends GameObject {
     private final Hero hero;
+    private final DyehardRectangle laserReverse;
 
     public Laser(Hero hero) {
         this.hero = hero;
@@ -18,8 +20,16 @@ public class Laser extends GameObject {
         texture = BaseCode.resources
                 .loadImage("Textures/Background/DeathEdge.png");
         setPanning(true);
-        setPanningSheet(texture, 4, 4, true);
+        setPanningSheet(texture, 16, 3, true);
         alwaysOnTop = true;
+
+        laserReverse = new DyehardRectangle();
+        laserReverse.size.set(width, height);
+        laserReverse.center.set(width / 2 - 2, height / 2);
+        laserReverse.setPanning(true);
+        laserReverse.reverse = true;
+        laserReverse.setPanningSheet(texture, 32, 3, true);
+        laserReverse.alwaysOnTop = true;
     }
 
     // TODO should we put this collision into the user code?
@@ -28,5 +38,11 @@ public class Laser extends GameObject {
         if (hero.center.getX() < center.getX()) {
             hero.kill(this);
         }
+    }
+
+    @Override
+    public void destroy() {
+        laserReverse.destroy();
+        super.destroy();
     }
 }
