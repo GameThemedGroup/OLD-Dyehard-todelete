d2eeb2d4b52af401c2e7590318c40b58f5d3fd09
Chuan Wang <chuan.wang101@gmail.com>
2014-11-21 03:13:58 -0800

    cheat start up with exec to increase memory heap

--- Java/DyeHardUser/src/Main.java
+++ Java/DyeHardUser/src/Main.java
@@ -4,7 +4,30 @@ public class Main extends GameWindow {
     private static final long serialVersionUID = 1L;
 
     public static void main(String[] args) throws Exception {
-        (new Main()).startProgram();
+        if (args.length == 0) {
+            String javaHome = System.getProperty("java.home");
+            if (javaHome.contains("eclipse")) {
+                Process ps = Runtime.getRuntime().exec(
+                        "java -jar -Xms1024m -Xmx1150m DyeHard.jar command");
+                ps.waitFor();
+            } else {
+                Process ps = Runtime
+                        .getRuntime()
+                        .exec(javaHome
+                                + " -jar -Xms1024m -Xmx1150m DyeHard.jar command");
+                ps.waitFor();
+            }
+        } else {
+            (new Main()).startProgram();
+        }
+        // System.out.println(heapMaxSize + "  " + heapFreeSize);
+        // (new Main()).startProgram();
+        //
+        // heapMaxSize = Runtime.getRuntime().maxMemory() / 1024 / 1024;
+        // heapFreeSize = Runtime.getRuntime().freeMemory() / 1024 / 1024;
+        //
+        // System.out.println(heapMaxSize + "  " + heapFreeSize);
+        // }
     }
 
     public Main() {
